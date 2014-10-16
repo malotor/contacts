75c0925ff2a4f5b11d0a46f2db87de709b6aa4ed
Manel <malotor@gmail.com>
2014-10-17 00:02:29 +0200

    Add gulp tasks and templates

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    rename = require('gulp-rename');
+
+gulp.task('styles', function() {
+    return gulp.src('public/components/bootstrap-sass-official/vendor/assets/stylesheets/*.scss')
+        .pipe(sass({ style: 'expanded' }))
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+        .pipe(gulp.dest('public/assets/css'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss())
+        .pipe(gulp.dest('public/assets/css'));
+})
+
+gulp.task('watch', function() {
+    gulp.watch('sass/*.scss', ['styles']);
+});
+

0ae881f39cc3c7981bb62acc592e986efcefd555
Manel <malotor@gmail.com>
2014-10-22 00:33:45 +0200

    Refactor contact validator

--- app/src/ContactDAO.php
+++ app/src/ContactDAO.php
@@ -2,6 +2,49 @@
 
 namespace malotor\contacts;
 
+abstract class Validation {
+
+  abstract public function validate();
+}
+
+abstract class ValidationDecorator extends Validation {
+  public function __construct($contactValidation, $value) {
+    $this->contactValidation = $contactValidation;
+    $this->value = $value;
+  }
+}
+
+class ContactValidation extends Validation {
+
+  public $contact;
+
+  public function __construct($contact) {
+    $this->contact = $contact;
+  }
+  public function validate() {
+    return true;
+  }
+}
+
+class ContactNameValidation extends ValidationDecorator {
+
+  public function validate() {
+
+    $validation = preg_match('/' . $this->value . '/', $this->contactValidation->contact->name);
+    
+    return $this->contactValidation->validate() &&  $validation ;
+  }
+}
+
+class ContactGenderValidation extends ValidationDecorator {
+
+  public function validate() {
+
+    $validation = ($this->contactValidation->contact->gender == $this->value);
+
+    return $this->contactValidation->validate() &&  $validation ;
+  }
+}
 
 class ContactDAO extends Singleton {
   protected $contacts;
@@ -31,16 +74,17 @@ class ContactDAO extends Singleton {
   public function searchContacts($search_params) {
     $result = array();
     foreach($this->contacts as $contact) {
-      $allChecksPass = true;
-      foreach($search_params as $param_key => $param_value) {
 
-        if ($contact->{$param_key} != $param_value ) {
-          $allChecksPass = false;
-        }
-      }
-      if ($allChecksPass) $result[] = $contact;
+      $contactValidation = new ContactValidation($contact);
+      if (isset($search_params['name'])) $contactValidation = new ContactNameValidation($contactValidation, $search_params['name']);
+      if (isset($search_params['gender'])) $contactValidation = new ContactGenderValidation($contactValidation, $search_params['gender']);
 
+      var_dump($contact);
+      var_dump($contactValidation->validate());
+
+      if ($contactValidation->validate()) $result[] = $contact;
     }
+
     return $result;
   }
 }
